b5bc5090f86611d1cc7f3dd67087d095c1a658f4
joseph <josephkusaytetteh@gmail.com>
2020-10-19 02:49:59 +0000

    Fixed class register bugs

--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -36,7 +36,7 @@ class S {
   /// `Submit assignment`
   String get submittedAssignments {
     return Intl.message(
-      'Submitted assignment',
+      'Submit assignment',
       name: 'submittedAssignments',
       desc: '',
       args: [],
